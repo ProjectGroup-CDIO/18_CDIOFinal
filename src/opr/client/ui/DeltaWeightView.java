9e83e7fe4f4ce0221547408ea98d4da07e84a434
Martin Vieth <martin_123_vieth@hotmail.com>
2015-06-12 10:28:59 +0200

    adding a celllist - test

--- src/opr/client/ui/DeltaWeightView.java
+++ src/opr/client/ui/DeltaWeightView.java
@@ -1,8 +1,13 @@
 package opr.client.ui;
 
+import java.util.Arrays;
+import java.util.List;
+
 import opr.client.service.IASEServiceAsync;
 import opr.client.ui.WeightView.Callback;
 
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -15,7 +20,7 @@ public class DeltaWeightView extends Composite{
 	private HorizontalPanel hPanel1 = new HorizontalPanel();
 	private HorizontalPanel hPanel2 = new HorizontalPanel();
 	private HorizontalPanel hPanel3 = new HorizontalPanel();
-	FlexTable ft = new FlexTable();
+	private FlexTable ft = new FlexTable();
 	private Label viewInfo = new Label("Delta Weight");
 	
 	private Label prdName = new Label("Product Name");
@@ -46,9 +51,33 @@ public class DeltaWeightView extends Composite{
 		ft.setWidget(1, 0, productName);
 		ft.setWidget(1, 1, productNr);
 		ft.setWidget(1, 2, weightData);
+		productListView();
 		
 		
 		
+	}
+	
+	
+	
+	public void productListView(){
+		
+		  // The list of data to display.
+		  final List<String> DAYS = Arrays.asList("Sunday", "Monday",
+		      "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+		    // Create a cell to render each value in the list.
+		    TextCell textCell = new TextCell();
+
+		    // Create a CellList that uses the cell.
+		    CellList<String> cellList = new CellList<String>(textCell);
+
+		    // Set the total row count. This isn't strictly necessary, but it affects
+		    // paging calculations, so its good habit to keep the row count up to date.
+		    cellList.setRowCount(DAYS.size(), true);
+
+		    // Push the data into the widget.
+		    cellList.setRowData(0, DAYS);
+		    ft.setWidget(1,2,cellList);
 		
 	}
 }
