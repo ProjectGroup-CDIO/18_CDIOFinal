e2f49cf8bc4a793ebf1ab755452195e8d6fe3a8f
thomas <thomas@liljegren.dk>
2015-06-14 14:15:23 +0200

    prøv nu yo

--- src/opr/server/interfaces/ICoinDAO.java
+++ src/opr/server/interfaces/ICoinDAO.java
@@ -1,8 +1,11 @@
 package opr.server.interfaces;
 
+import java.util.List;
+
 import opr.shared.CoinDTO;
 import opr.shared.DALException;
 
 public interface ICoinDAO {
 	CoinDTO getCoinInfo(double value) throws DALException;
+	List<CoinDTO> getCoinList() throws DALException;
 }
