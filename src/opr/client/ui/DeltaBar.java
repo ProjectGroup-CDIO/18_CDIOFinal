b70746816fd14d2842c7fef99e9004056a1dd74e
Kenneth Frandsen <Frandsen86@gmail.com>
2015-06-15 14:25:14 +0200

    Ændring af faver i Deltabar.

--- src/opr/client/ui/DeltaBar.java
+++ src/opr/client/ui/DeltaBar.java
@@ -18,32 +18,33 @@ public class DeltaBar extends Composite {
 
 	public DeltaBar(final MainView main){
 		
-		double e1 = 200.0;
-		double e2 = 228.0;
-		double s = 150.0;
+		double e1 = 191.0;
+		double e2 = 211.0;
+		double s = 201.0;
 		
 		LayoutPanel layoutPanel = new LayoutPanel();
-		layoutPanel.setStyleName("layoutPanel0");
+		layoutPanel.setStyleName("h1");
 		initWidget(layoutPanel);
-		layoutPanel.setSize("403px", "37px");
+		layoutPanel.setSize("402px", "37px");
 		
 		LayoutPanel layoutPanel_1 = new LayoutPanel();
 		layoutPanel_1.setStyleName("layoutPanel2");
 		layoutPanel.add(layoutPanel_1);
-		layoutPanel.setWidgetLeftWidth(layoutPanel_1, e1, Unit.PX, 2.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(layoutPanel_1, s, Unit.PX, 2.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(layoutPanel_1, 0.0, Unit.PX, 37.0, Unit.PX);
 		
 		LayoutPanel layoutPanel_2 = new LayoutPanel();
 		layoutPanel_2.setStyleName("layoutPanel1");
 		layoutPanel.add(layoutPanel_2);
-		layoutPanel.setWidgetLeftWidth(layoutPanel_2, e2, Unit.PX, 2.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(layoutPanel_2, e1, Unit.PX, 2.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(layoutPanel_2, 0.0, Unit.PX, 37.0, Unit.PX);
 		
 		LayoutPanel layoutPanel_3 = new LayoutPanel();
 		layoutPanel_3.setStyleName("layoutPanel1");
 		layoutPanel.add(layoutPanel_3);
-		layoutPanel.setWidgetLeftWidth(layoutPanel_3, 150.0, Unit.PX, 2.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(layoutPanel_3, e2, Unit.PX, 2.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(layoutPanel_3, 0.0, Unit.PX, 37.0, Unit.PX);
+	
 		
 	}
 }
