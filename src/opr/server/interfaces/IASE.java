f694ee846c20d5ebcb19db00d4acf92e81322899
naelrashdeen <NaelRashdeen@10.16.164.39>
2015-06-17 10:03:43 +0200

    Tilføjet kommando til weight overload.

--- src/opr/server/interfaces/IASE.java
+++ src/opr/server/interfaces/IASE.java
@@ -3,12 +3,14 @@ package opr.server.interfaces;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import opr.shared.DALException;
+
 public interface IASE extends Runnable {
 	
 	void setWeightDisplay(String msg);
 	void connect() throws UnknownHostException, IOException;
 	double getSWeight() throws IOException;
-	double getSIWeight() throws IOException;
+	double getSIWeight() throws IOException, DALException;
 	void tara() throws Exception;
 //	void run();
 	
