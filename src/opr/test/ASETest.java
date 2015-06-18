f1296438f0c809d13be8ad3bab852c5ac91cc242
Pede <Pede@10.16.106.82>
2015-06-18 11:03:40 +0200

    ASETest færdig

--- src/opr/test/ASETest.java
+++ src/opr/test/ASETest.java
@@ -1,99 +1,82 @@
 package opr.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import opr.server.impl.ASE;
+import opr.shared.DALException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ASETest {
 
+	static ASE ase;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		ase = new ASE();
+		ase.connect();
+	}
+
+
+	@Test
+	public void testConnect() throws UnknownHostException, IOException {
+		assertTrue(ase.getSock().isConnected());
+	}
+
+
+	@Test
+	public void testGetSWeight() throws IOException {
+		double weight = ase.getSWeight();
+		boolean weightBoo = false;
+		
+		if(weight >= 0){
+			weightBoo = true;
+		}
+		
+		assertTrue(weightBoo);
 	}
 
 	@Test
-	public void testConnect() {
-		fail("Not yet implemented");
+	public void testGetSIWeight() throws IOException, DALException {
+		double weight = ase.getSIWeight();
+		boolean weightBoo = false;
+		
+		if(weight >= 0){
+			weightBoo = true;
+		}
+		
+		assertTrue(weightBoo);
 	}
 
 	@Test
-	public void testSetWeightDisplay() {
-		fail("Not yet implemented");
+	public void testTara() throws Exception {
+		boolean taraBoo = false;
+		ase.getSWeight();
+		ase.tara();
+		
+		if(ase.getNetto() == 0){
+			taraBoo = true;
+		}
+		
+		assertTrue(taraBoo);
 	}
 
+	
 	@Test
-	public void testGetSWeight() {
-		fail("Not yet implemented");
+	public void testReturnNetto() throws Exception {
+		boolean nettoBoo = false;
+		ase.setBrutto(4.75);
+		ase.tara();
+		double nettoTest = ase.returnNetto();
+		
+		if(nettoTest == 0){
+			nettoBoo = true;
+		}
+		
+		assertTrue(nettoBoo);
 	}
-
-	@Test
-	public void testGetSIWeight() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testTara() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSock() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetSock() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetIn() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetIn() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetOut() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetOut() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetBrutto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetBrutto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNetto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetNetto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testReturnNetto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testChangeSocket() {
-		fail("Not yet implemented");
-	}
-
 }
