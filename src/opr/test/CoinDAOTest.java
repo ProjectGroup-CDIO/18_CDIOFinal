fadb2713aa64908a00ef6fca1e50067dbcada250
Martin Vieth <martin_123_vieth@hotmail.com>
2015-06-18 11:00:54 +0200

    BatchDAOTest er done, og coinDAO test

--- src/opr/test/CoinDAOTest.java
+++ src/opr/test/CoinDAOTest.java
@@ -2,23 +2,34 @@ package opr.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import opr.server.impl.BatchDAO;
+import opr.server.impl.CoinDAO;
+import opr.shared.CoinDTO;
+import opr.shared.DALException;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CoinDAOTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	CoinDAO cDAO;
+	@Before
+	public void setUpBefore() throws Exception {
+		cDAO = new CoinDAO();
 	}
 
 	@Test
-	public void testGetCoinInfo() {
-		fail("Not yet implemented");
+	public void testGetCoinInfo() throws DALException {
+		CoinDTO cDTO=cDAO.getCoinInfo(1);
+		assertTrue(cDTO.getWeightPerUnit()> 0);
 	}
 
 	@Test
-	public void testGetCoinList() {
-		fail("Not yet implemented");
+	public void testGetCoinList() throws DALException {
+		List<CoinDTO> cDTO=cDAO.getCoinList();
+		assertTrue(cDTO.get(0).getWeightPerUnit()> 0);
 	}
 
 }
--- src/opr/test/CondimentsDAOTest.java
+++ src/opr/test/CondimentsDAOTest.java
@@ -1,19 +1,26 @@
 package opr.test;
 
 import static org.junit.Assert.*;
+import opr.server.impl.BatchDAO;
+import opr.server.impl.CondimentsDAO;
+import opr.shared.DALException;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CondimentsDAOTest {
+	CondimentsDAO cDAO;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUpBefore() throws Exception {
+		cDAO = new CondimentsDAO();
 	}
 
+
 	@Test
-	public void testGetCondimentsInfo() {
-		fail("Not yet implemented");
+	public void testGetCondimentsInfo() throws DALException {
+		cDAO.getCondimentsInfo("Salt");
 	}
 
 	@Test
