7591316606f1be3d8f1714811a45dcfea3e2e4e6
Martin Vieth <martin_123_vieth@hotmail.com>
2015-06-18 11:39:18 +0200

    FruitDAOTest og CondimentsTest er done

--- src/opr/test/CondimentsDAOTest.java
+++ src/opr/test/CondimentsDAOTest.java
@@ -1,8 +1,12 @@
 package opr.test;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import opr.server.impl.BatchDAO;
 import opr.server.impl.CondimentsDAO;
+import opr.shared.CondimentsDTO;
 import opr.shared.DALException;
 
 import org.junit.Before;
@@ -20,12 +24,17 @@ public class CondimentsDAOTest {
 
 	@Test
 	public void testGetCondimentsInfo() throws DALException {
-		cDAO.getCondimentsInfo("Salt");
+	CondimentsDTO cDTO = cDAO.getCondimentsInfo("Salt");
+		assertTrue(cDTO.getName().equals("Salt"));
+		assertTrue(cDTO.getTolerance() == 0.02);
 	}
 
 	@Test
-	public void testGetCondimentsList() {
-		fail("Not yet implemented");
+	public void testGetCondimentsList() throws DALException {
+	List<CondimentsDTO> ListCDTO = cDAO.getCondimentsList();
+	assertTrue(ListCDTO.get(0).getId() == 7);
+	assertTrue(ListCDTO.get(0).getName().equals("Salt"));
+	assertTrue(ListCDTO.get(0).getTolerance() == 0.02);
 	}
 
 }
